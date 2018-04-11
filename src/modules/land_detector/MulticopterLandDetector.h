2384d41ec54d1d660599ff6aaed6f09418bc79ce
Daniel Agar <daniel@agar.ca>
2018-04-11 15:10:51 -0400

    WIP cmake architecture

--- src/modules/land_detector/MulticopterLandDetector.h
+++ src/modules/land_detector/MulticopterLandDetector.h
@@ -44,7 +44,7 @@
 
 #include "LandDetector.h"
 
-#include <systemlib/param/param.h>
+#include <parameters/param.h>
 #include <uORB/topics/vehicle_local_position.h>
 #include <uORB/topics/vehicle_local_position_setpoint.h>
 #include <uORB/topics/vehicle_attitude.h>
